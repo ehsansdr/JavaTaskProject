1d6d9ccea8ff8bc23f6482972c5be296ac9fba04
ehsansdr <sehsan251@gmail.com>
2024-05-17 14:11:39 +0330

    JwtService finished

--- src/main/java/com/example/JavaTaskProject/config/JwtService.java
+++ src/main/java/com/example/JavaTaskProject/config/JwtService.java
@@ -2,11 +2,17 @@ package com.example.JavaTaskProject.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 /** we need the class that can manipulate this jwt token */
 
@@ -22,8 +28,54 @@ public class JwtService {
     private static final String SECRET_KEY = "a9d796fb824461f486fd5e0bba1050f010b9b06ca04578a1daeb00360b785554";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject); // subject is the email or username of the user
     }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token); // so first extract all claims
+        return claimsResolver.apply(claims);
+    }
+
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    // in this method we can declare the time of validation
+    public String generateToken(
+            Map<String, Object> extraClaims,  // this map will contain the claim and extra claim that we want to add
+            UserDetails userDetails
+    ) {
+        // todo : declare the time of validation of token in this return part
+        return Jwts
+                .builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis())) // the issue date (when token create I think)
+                .setExpiration(new Date(System.currentTimeMillis() + (1000  * 60 * 60))) // 1s * 60(1 minute contains 60 sec) * 60(minutes)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact(); // generate and create token
+    }
+
+    // we want to validate if this  token right here belongs to this UserDetails
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        //we get username to compare and ,make sure about that detail
+        final String username = extractUsername(token);
+
+        // if the username that we get from of token the username is same as username we have as input
+        // and we should to make sure the validation of token and that is not expired
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    // we return boolean for mke sure the toke is not expired by using method that return expiration date
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());  // we want to make sure it is today's date
+    }
+
+    // this method return the expiration date
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
 
     private Claims extractAllClaims(String token) {
         /* what is sign in key ?                      (55:00)
